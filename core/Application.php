27d723154f1609979a510ebe773e8aef45db704c
Juan Pablo Villafáñez <jvillafanez@solidgeargroup.com>
2019-05-02 15:22:33 +0200

    Show summary with the state of the users and send one-time notification
     Conflicts:
    	core/register_command.php

--- core/Application.php
+++ core/Application.php
@@ -39,6 +39,7 @@ use OC\Core\Controller\RolesController;
 use OC\Core\Controller\TokenController;
 use OC\Core\Controller\TwoFactorChallengeController;
 use OC\Core\Controller\UserController;
+use OC\Core\Notification\Notifier;
 use OC_Defaults;
 use OCP\AppFramework\App;
 use OCP\BackgroundJob\IJobList;
@@ -46,6 +47,7 @@ use OCP\IConfig;
 use OCP\ILogger;
 use OCP\IServerContainer;
 use OCP\Util;
+use OCP\Notification\Events\RegisterNotifierEvent;
 
 /**
  * Class Application
@@ -213,6 +215,15 @@ class Application extends App {
 		});
 		$container->registerService('TwoFactorAuthManager', static function (SimpleContainer $c) {
 			return $c->query('ServerContainer')->getTwoFactorAuthManager();
+		});
+	}
+
+	public function registerNotifier() {
+		$container = $this->getContainer();
+		$dispatcher = $container->getServer()->getEventDispatcher();
+		$dispatcher->addListener(RegisterNotifierEvent::NAME, function (RegisterNotifierEvent $event) use ($container) {
+			$l10n = $container->getServer()->getL10N('core');
+			$event->registerNotifier($container->query(Notifier::class), 'core', $l10n->t('Core'));
 		});
 	}
 }
